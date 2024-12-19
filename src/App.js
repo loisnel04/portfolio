9d882bea80730e8240c9a315b75aa8153109cc14
Loïs <lois.isnel@epitech.eu>
2024-12-19 15:21:22 +0100

    header updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Footer from './Footer';
 import Header from './Header';
+import Body from './Body';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Body />
       <Footer />
     </div>
   );
--- src/Header.js
+++ src/Header.js
@@ -2,6 +2,7 @@ import './Header.css'
 import computer from "./img/logo_header.avif"
 import france_flag from "./img/drapeau-france.png"
 import english_flag from "./img/drapeau-anglais.png"
+import download from './img/download.webp'
 
 export default function Header() {
     const handleLangue = () => {
@@ -24,6 +25,9 @@ export default function Header() {
                 <p>My name is Loïs Isnel, i am a young developper, studying at <a href="https://www.epitech.eu/ecole-informatique-marseille/" target="_blank" rel="noreferrer">Marseille Epitech</a> and alternating at <a href="https://afmicado.com/" target="_blank" rel="noreferrer">Micado company</a>.</p>
                 <p>I learn AI, web/Application development, and Data engineering to become a IT engineer in one of this domain.</p>
                 <p>I really love computer science, and science in general. My goal is to learn more of it and working to create software which help people.</p>
+            </div>
+            <div id="download_button">
+                <button source={download}>My CV<img src={download} alt="Download"></img></button>
             </div>
             <br></br>
             <br></br>
