558807d3d6eba1541814ade434df36773c14189a
Loïs <lois.isnel@epitech.eu>
2024-12-30 10:46:21 +0100

    Add navbar

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import './App.css';
 import Footer from './Footer';
 import Header from './Header';
 import Body from './Body';
+import Navbar from './Navbar';
+import { BrowserRouter as Routers, Routes, Route} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <div id="separator"></div>
+      <Navbar />
       <div id="separator"></div>
       <Body />
       <div id="separator"></div>
---
+++ src/Navbar.js
@@ -0,0 +1,11 @@
+import Header from "./Header"
+import React from "react"
+
+export default function Navbar() {
+    return (
+        <div id="Navbar">
+            <a href="header">Header</a>
+
+        </div>
+    )
+}
